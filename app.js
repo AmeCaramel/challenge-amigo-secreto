98702295d5b592931f40f4aea332d3ffcf753a09
AmeCaramel <correammm@gmail.com>
2025-09-08 20:37:46 -0300

    Añade README y actualiza código en HTML Y js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const listaAmigos = document.getElementById('listaAmigos'); 
+const inputAmigo = document.getElementById('amigo'); 
+const resultado = document.getElementById('resultado');
+
+function agregarAmigo() {
+    const nombre = inputAmigo.value; 
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return; 
+    }
+    amigos.push(nombre); 
+
+    inputAmigo.value = '';
+
+    actualizarLista();
+}
+
+function actualizarLista() {
+    listaAmigos.innerHTML = ''; 
+    for (let indice = 0; indice < amigos.length; indice++) {
+        const item = document.createElement("li");
+            item.textContent = amigos[indice];
+            listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {            
+        alert('No hay amigos para sortear. Por favor, añadí al menos un nombre.');
+        return;   
+    }
+  
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];    
+
+    resultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSorteado}</li>`;
+}
+
